ae88403a4b5143e68943acbe8999e94c81e6a394
Arishemm <arishem51@gmail.com>
2023-07-03 19:19:57 +0700

    feat: formatting java by red hat

--- Hello/Hello.java
+++ Hello/Hello.java
@@ -1,8 +1,9 @@
 package Hello;
+
 public class Hello {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         System.out.println("Hello World Java!");
         System.out.println("How are you?");
-            System.out.print("This is line");
+        System.out.print("This is line");
     }
 }
