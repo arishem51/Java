8d27aa2092ed4fcf8b81a96b376b2b6e42f2d67d
Arishemm <arishem51@gmail.com>
2023-07-03 19:14:43 +0700

    chore: simple command

--- Hello/Hello.java
+++ Hello/Hello.java
@@ -2,5 +2,7 @@ package Hello;
 public class Hello {
     public static void main(String[] arg){
         System.out.println("Hello World Java!");
+        System.out.println("How are you?");
+            System.out.print("This is line");
     }
 }
