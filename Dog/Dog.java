8ce2e9b8275af07a29a91a59e78015332dd5eb78
Arishemm <arishem51@gmail.com>
2023-07-12 15:08:24 +0700

    feat: Dog test drive

--- Dog/Dog.java
+++ Dog/Dog.java
@@ -1,7 +1,19 @@
 package Dog;
 
-public class Dog {
+class Dog {
+    int size;
+    String bread;
+    String name;
+
     void bark() {
-        System.out.printf("I'm a god and i will bark you!");
+        System.out.printf("Ruff ruff!");
+    }
+}
+
+class DogTestDrive {
+    public static void main(String[] arg) {
+        Dog myDog = new Dog();
+        myDog.size = 40;
+        myDog.bark();
     }
 }
