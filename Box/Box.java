8168d4678161778a1b4016cc262777550403e497
Arishemm <arishem51@gmail.com>
2023-08-13 18:47:07 +0700

    feat: Overload Java

--- Box/Box.java
+++ Box/Box.java
@@ -4,6 +4,7 @@ class Shape {
     double width;
     double height;
     double depth;
+    static final int NOT_INITIALIZE = -1;
 
     double volume() {
         return depth * height * width;
@@ -14,12 +15,27 @@ class Shape {
         this.height = height;
         this.depth = depth;
     }
+
+    Shape() {
+        this.width = NOT_INITIALIZE;
+        this.height = NOT_INITIALIZE;
+        this.depth = NOT_INITIALIZE;
+    }
+
+    Shape(double length) {
+        this.width = length;
+        this.height = length;
+        this.depth = length;
+    }
 }
 
 public class Box {
     public static void main(String arg[]) {
         Shape myShape = new Shape(5, 2, 3);
-        double volume = myShape.volume();
-        System.out.println("Volume is: " + volume);
+        Shape myCube = new Shape(5);
+        Shape noDimShape = new Shape();
+        System.out.println("Volume is: " + myShape.volume());
+        System.out.println("Volume is: " + myCube.volume());
+        System.out.println("Volume is: " + noDimShape.volume());
     }
 }
