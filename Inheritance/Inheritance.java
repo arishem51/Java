bd42af78d566e15c4081bf9dd19710ae1f1cb563
Arishemm <arishem51@gmail.com>
2023-08-14 19:40:18 +0700

    feat: Supẻ

--- Inheritance/Inheritance.java
+++ Inheritance/Inheritance.java
@@ -4,9 +4,13 @@ class A {
     int i;
     private int j;
 
-    void setIJ(int i, int j) {
+    private void setIJ(int i, int j) {
         this.i = i;
         this.j = j;
+    }
+
+    A(int i, int j) {
+        setIJ(i, j);
     }
 
     void showIJ() {
@@ -17,29 +21,28 @@ class A {
 class B extends A {
     int k;
 
-    void showK() {
-        System.out.println("K: " + k);
+    B(int i, int j, int k) {
+        super(i, j);
+        this.k = k;
     }
 
-    void setIJK(int i, int k, int j) {
-        this.setIJ(i, j);
-        this.k = k;
+    void showEl() {
+        this.showIJ();
+        System.out.println("K: " + this.k);
     }
 }
 
 public class Inheritance {
     public static void main(String args[]) {
-        A classA = new A();
-        B classB = new B();
+        A classA = new A(10, 20);
+        B classB = new B(5, 2, 2);
 
-        classA.setIJ(10, 20);
         System.out.println("This is class A: ");
         classA.showIJ();
         System.out.println();
 
-        classB.setIJK(5, 7, 2);
         System.out.println("This is class B: ");
-        classB.showIJ();
+        classB.showEl();
         System.out.println();
 
         // WTF dude?
