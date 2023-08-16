51a3c6318de63d15ff5150d257caafc0de30dfb3
Arishemm <arishem51@gmail.com>
2023-08-16 19:30:50 +0700

    chore: Apple instance

--- Enumerations/Enumerations.java
+++ Enumerations/Enumerations.java
@@ -20,9 +20,11 @@ enum Apple {
 
 public class Enumerations {
     public static void main(String args[]) {
-        Apple ap;
+        Apple ap = Apple.Hung;
 
         System.out.println("Hung apples: " + Apple.Hung.getPrice());
+        System.out.println(ap.equals(Apple.Anh));
+        System.out.println(ap.equals(Apple.Hung));
 
         for (Apple item : Apple.values()) {
             System.out.println(item + " costs " + item.getPrice());
